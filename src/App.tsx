d3544f5906339a6622b51bc208531780be64cdeb
turtlemoji <78362532+turtlemoji@users.noreply.github.com>
2023-04-18 14:00:21 -0300

    feat: init setup (#1)
    Fixes CRO-1

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
