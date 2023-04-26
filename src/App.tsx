c47bdd6de94c51999369cd81790751b55d3576a0
Ardy <116569704+0xArdy@users.noreply.github.com>
2023-04-26 14:57:24 -0400

    fix: test app (#17)
    closes CRO-27

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,6 @@ function App() {
         value={userAddress}
         onChange={(e) => setUserAddress(e.target.value)}
       />
-      <ZodiacConnextWidget
-        originAddress="0x0000000123123131"
-        originChainId={55}
-        text="Open Modal"
-      />
       {!isAddress(userAddress) && <p>invalid address</p>}
       <br />
       <br />
@@ -38,15 +33,6 @@ function App() {
           modal={useModal}
         />
       )}
-
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
